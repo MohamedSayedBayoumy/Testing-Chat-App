850bd37b707d10a0d12490eba72ac4de0bf95901
mohamedSayedBayoumy <mandobobo12345@gmail.com>
2026-08-19 23:47:15 +0300

    chore: integrate FVM for Flutter SDK management and add test infrastructure dependencies

---
+++ test/features/chat/chat_screen_test.dart
@@ -0,0 +1,10 @@
+import 'package:chat_app/features/chat/cubit/chat_cubit.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:bloc_test/bloc_test.dart';
+
+class MockChatCubit extends MockCubit<ChatState> implements ChatCubit {}
+
+void main() {
+  testWidgets('chat screen ...', (tester) async {});
+}
