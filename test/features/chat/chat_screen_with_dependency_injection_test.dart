a31073cdcdb2568eca7d79a7baad7986756cec28
mohamedSayedBayoumy <mandobobo12345@gmail.com>
2026-08-30 04:57:28 +0300

    test: add widget tests for chat screen with dependency injection

--- test/features/chat/chat_screen_with_dependency_injection_test.dart
+++ test/features/chat/chat_screen_with_dependency_injection_test.dart
@@ -1,11 +1,14 @@
+import 'package:chat_app/core/error/common_failed_model.dart';
 import 'package:chat_app/features/chat/chat_screen.dart';
 import 'package:chat_app/features/chat/cubit/chat_cubit.dart';
 import 'package:chat_app/features/chat/models/gemini_request_model.dart';
 import 'package:chat_app/features/chat/models/gemini_response_model.dart';
 import 'package:chat_app/features/chat/models/message_model.dart';
 import 'package:chat_app/features/chat/repos/chat_repos.dart';
+import 'package:chat_app/features/chat/widgets/chat_bubble.dart';
 import 'package:chat_app/service/service_locator.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -26,6 +29,15 @@ GeminiResponse _geminiResponse() => GeminiResponse(
   ],
 );
 
+// DioException _retryableDioException() => DioException(
+//   type: DioExceptionType.sendTimeout,
+//   requestOptions: RequestOptions(),
+// );
+
+DioException _nonRetryableDioException() => DioException(
+  type: DioExceptionType.connectionError,
+  requestOptions: RequestOptions(),
+);
 void main() {
   late ChatRepositoryMock chatRepositoryMock;
 
@@ -67,7 +79,7 @@ void main() {
 
       await tester.enterText(textField, "Hello");
 
-      // this timer cause we have startListening() it listen to text filed every milliseconds: 100 
+      // this timer cause we have startListening() it listen to text filed every milliseconds: 100
       await tester.pump(const Duration(milliseconds: 150));
 
       final sendIcon = find.byKey(Key('send_icon'));
@@ -80,6 +92,106 @@ void main() {
       expect(loadingFiled, findsOneWidget);
 
       await tester.pumpAndSettle(const Duration(seconds: 4));
+    });
+
+    testWidgets("Failed With non-Retirable Exception", (tester) async {
+      when(
+        () => chatRepositoryMock.sendMessage(
+          requestBody: any(named: "requestBody"),
+        ),
+      ).thenAnswer((_) async {
+        return Left(
+          DioFailure(
+            failureMessage: "Error",
+            modelException: _nonRetryableDioException(),
+            failureMessageTitle: "Error Title",
+          ),
+        );
+      });
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: BlocProvider<ChatCubit>(
+            create: (context) => serviceLocator<ChatCubit>()..startListening(),
+            child: ChatScreen(),
+          ),
+        ),
+      );
+      await tester.pump();
+
+      var textFiled = find.byKey(Key("text_field"));
+      await tester.enterText(textFiled, "Hi");
+      await tester.pump(Duration(milliseconds: 159));
+
+      var sendIcon = find.byKey(Key("send_icon"));
+      await tester.tap(sendIcon);
+      await tester.pump();
+
+      final cubit = serviceLocator<ChatCubit>();
+
+      var errorBar = find.byType(SnackBar);
+
+      expect(errorBar, findsOneWidget);
+      expect(cubit.state, isA<MessageFailed>());
+
+      await tester.pump(const Duration(milliseconds: 222));
+    });
+
+    testWidgets("Success Case and show response Message", (tester) async {
+      when(
+        () => chatRepositoryMock.sendMessage(
+          requestBody: any(named: "requestBody"),
+        ),
+      ).thenAnswer((_) async {
+        return Right(_geminiResponse());
+      });
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: BlocProvider<ChatCubit>(
+            create: (context) => serviceLocator<ChatCubit>()..startListening(),
+            child: ChatScreen(),
+          ),
+        ),
+      );
+      await tester.pump();
+
+      var textFiled = find.byKey(Key("text_field"));
+      await tester.enterText(textFiled, "Hi");
+      await tester.pump(Duration(milliseconds: 159));
+
+      var sendIcon = find.byKey(Key("send_icon"));
+      await tester.tap(sendIcon);
+      await tester.pump(); // one to show loading 
+      await tester.pump(); // one to show message bubble
+
+      final cubit = serviceLocator<ChatCubit>();
+      expect(cubit.state, isA<MessageSended>());
+
+      final getLastMessageBubble = find.byType(ChatBubble).first;
+      // ❗️⚠️💡🚨 Note (VERY IMPORTANT 🚨💡⚠️❗️): 
+      // We use `.first` instead of `.last` because the Chat ListView has `reverse: true` look at => lib\features\chat\widgets\messages_list_widget.dart : Line 19
+      // This means the "newest" message is rendered as the very "first" widget in the tree (Index 0).
+      // Using `.last` would return the oldest message in the chat history.
+
+      final getContainerOfLastMessageBubble = find.descendant(
+        of: getLastMessageBubble,
+        matching: find.byType(DecoratedBox),
+      );
+
+      final getPropertiesOfContainerOfLastMessageBubble = tester
+          .widget<DecoratedBox>(getContainerOfLastMessageBubble.first);
+
+      final castingPropertiesOfContainerOfLastMessageBubble =
+          getPropertiesOfContainerOfLastMessageBubble.decoration
+              as BoxDecoration;
+
+      expect(
+        castingPropertiesOfContainerOfLastMessageBubble.color,
+        const Color(0xFFF3F3F3),
+      );
+
+      await tester.pump(const Duration(milliseconds: 222));
     });
   });
 }
